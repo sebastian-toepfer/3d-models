edb2cdcfce2e0daabfd99d09502369b40ddfec9f
Sebastian Toepfer <61313468+sebastian-toepfer@users.noreply.github.com>
2023-09-12 17:17:40 +0200

    extend generation of stl with parameter support

--- generate-stls.sh
+++ generate-stls.sh
@@ -4,6 +4,17 @@ for file in ./designs/**/*.scad; do
     outputFileName=$(basename "$file")
     subDirs=$(dirname "$file" | sed "s/\.\/designs\///")
     mkdir -p generated/$subDirs
-    openscad -q -D \$fn=100 $file -o ./generated/$subDirs/$outputFileName.stl
+    parametersFileName=designs/$subDirs/$outputFileName.json
+    if [ -f $parametersFileName ];
+    then
+        configurations=$(jq -r ".parameterSets | to_entries[] | .key" $parametersFileName)
+        for configuration in $configurations
+        do
+            generatedFileName=./generated/$subDirs/$outputFileName.$configuration.stl
+            openscad -q -p $parametersFileName -P $configuration -D \$fn=100 $file -o $generatedFileName
+        done
+    else
+        openscad -q -D \$fn=100 -o ./generated/$subDirs/$outputFileName.stl $file
+    fi
 done
 
