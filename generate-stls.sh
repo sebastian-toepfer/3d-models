7d5f8bbfd6faabe37999504983c4e2ca1875a14d
Sebastian Toepfer <61313468+sebastian-toepfer@users.noreply.github.com>
2024-07-27 21:52:00 +0200

    replace generate of non param desgins with makefile generation

--- generate-stls.sh
+++ generate-stls.sh
@@ -3,6 +3,7 @@
 BASE_OPENSCAD_CMD="openscad --hardwarnings -D \$fn=128"
 mkdir -p generated/new_stls
 
+make all
 for file in $(find ./designs -type f -name *.scad); do
     outputFileName=$(basename "$file")
     subDirs=$(dirname "$file" | sed "s/\.\/designs\///")
@@ -27,20 +28,6 @@ for file in $(find ./designs -type f -name *.scad); do
                 fi
             fi
         done
-    else
-        generatedFileName=./generated/$subDirs/$outputFileName.stl
-        if [ ! -f $generatedFileName ] || [ $file -nt $generatedFileName ];
-        then
-            echo "generate $generatedFileName"
-            $BASE_OPENSCAD_CMD -o ./generated/$subDirs/$outputFileName.stl $file
-            retVal=$?
-            if [ $retVal -ne 0 ];
-            then
-                exit $retVal
-            else
-                cp $generatedFileName generated/new_stls/
-            fi
-        fi
     fi
 done
 
