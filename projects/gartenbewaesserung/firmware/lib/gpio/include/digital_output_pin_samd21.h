3a1d2b4a9bcd1c5f109b810dceaf4b8316a91a5e
Sebastian Toepfer <61313468+sebastian-toepfer@users.noreply.github.com>
2025-04-26 00:15:05 +0200

    [gartenbewaesserung(firmware)] add library to improve access to digital pins
    is a less mcu approach, we use switch_on to set high and switch_off to
    set low inset of pin_set and pin_clear or directly set false/true or
    high/low.
    open issue how to work with inverted systems like a relay which opens at
    low or clientselect in spi ...

---
+++ projects/gartenbewaesserung/firmware/lib/gpio/include/digital_output_pin_samd21.h
@@ -0,0 +1,24 @@
+#ifndef DIGITAL_OUTPUT_PIN_SAMD21_H
+#define DIGITAL_OUTPUT_PIN_SAMD21_H
+#include "sam.h"
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  typedef struct
+  {
+    PortGroup *group;
+    uint32_t pin_mask;
+    uint8_t pin_index;
+  } DigitalPinInfo_SAMD21;
+
+  struct DigitalOutputPin *
+  digital_output_pin_samd21_create(const DigitalPinInfo_SAMD21 *cfg);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
