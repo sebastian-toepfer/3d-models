0b7a14be38fc33ebe2d7164b2ebac8af32a3de1a
Sebastian Toepfer <61313468+sebastian-toepfer@users.noreply.github.com>
2025-07-09 18:40:26 +0200

    rename bewaesserung to pumpensteuerung and make it a subproject of bewaesserung

---
+++ projects/garten/bewaesserung/pumpensteuerung/firmware/src/connectivity/transceiver.h
@@ -0,0 +1,26 @@
+/*
+ * MIT License
+ * Copyright (c) 2025 Sebastian Toepfer
+ */
+#ifndef TRANSCEIVER_H
+#define TRANSCEIVER_H
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+struct Transceiver {
+    size_t (*write)(const struct Transceiver *self, const uint8_t *data, size_t len);
+    size_t (*read)(const struct Transceiver *self, uint8_t *data, size_t len);
+};
+
+size_t transceiver_write(const struct Transceiver *transceiver, const uint8_t *data, size_t len);
+size_t transceiver_read(const struct Transceiver *transceiver, uint8_t *data, size_t len);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
